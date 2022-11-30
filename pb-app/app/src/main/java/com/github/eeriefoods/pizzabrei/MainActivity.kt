920cee5e290c3c4af909e08dc0e1b7cdca90f579
Rexphel <philiprexroth@gmail.com>
2022-11-30 17:58:24 +0100

    Grundlage für Layout angefangen

--- pb-app/app/src/main/java/com/github/eeriefoods/pizzabrei/MainActivity.kt
+++ pb-app/app/src/main/java/com/github/eeriefoods/pizzabrei/MainActivity.kt
@@ -3,14 +3,20 @@ package com.github.eeriefoods.pizzabrei
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.eeriefoods.pizzabrei.ui.theme.PizzaBreiTheme
+import java.time.LocalTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +28,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    ListApps("COC")
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Scaffold(
+        topBar = { Top}
+    )
+}
+
+@Composable
+fun AppCard(){
+
+}
+
+@Composable
+fun ButtonNavigationBar(){
+
+}
+
+@Composable
+fun ListApps(name: String) {
+    Box{
+        Row() {
+            Image(
+                painterResource(R.drawable.ic_launcher_foreground),
+                "Home-button",
+                Modifier.border(1.5.dp, MaterialTheme.colorScheme.primary))
+            Column{
+                Text("Name: $name!")
+                Text("Time: ${LocalTime.now()}")
+                TextButton(onClick = { /*TODO*/ }) {
+                    Text("alloha")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PizzaBreiTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
--- pb-app/app/src/main/java/com/github/eeriefoods/pizzabrei/ui/theme/Theme.kt
+++ pb-app/app/src/main/java/com/github/eeriefoods/pizzabrei/ui/theme/Theme.kt
@@ -16,15 +16,20 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue,
+    secondary = LightBlue,
+    tertiary = DeepBlue,
+    background = Gray,
+    surface = Green
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue,
+    secondary = LightBlue,
+    tertiary = DeepBlue,
+    background = Gray,
+    surface = Green
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -40,6 +45,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun PizzaBreiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
