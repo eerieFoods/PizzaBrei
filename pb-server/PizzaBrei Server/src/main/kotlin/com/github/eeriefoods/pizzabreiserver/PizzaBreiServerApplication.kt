b65f8b34a4ddee51b3e6e756aec3ba1763f00113
JayReturns <jay3105@hotmail.com>
2022-12-01 10:13:09 +0100

    [#21] Added Swagger Infos

--- pb-server/PizzaBrei Server/src/main/kotlin/com/github/eeriefoods/pizzabreiserver/PizzaBreiServerApplication.kt
+++ pb-server/PizzaBrei Server/src/main/kotlin/com/github/eeriefoods/pizzabreiserver/PizzaBreiServerApplication.kt
@@ -1,8 +1,27 @@
 package com.github.eeriefoods.pizzabreiserver
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "PizzaBrei",
+        description = "Backand für Mobile Applikationen Vorlesung",
+        version = "v1",
+        contact = Contact(
+            url = "https://github.com/eerieFoods",
+            name = "eerieFoods"
+        ),
+        license = License(
+            name = "GNU General Public License v3.0",
+            url = "https://www.gnu.de/documents/gpl-3.0.en.html"
+        )
+    )
+)
 @SpringBootApplication
 class PizzaBreiServerApplication
 
