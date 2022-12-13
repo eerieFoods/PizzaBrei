32a7906341871924b683286592db2bd9a20d6dc8
JayReturns <jay3105@hotmail.com>
2022-12-13 20:08:22 +0100

    [#30] Centered text fields

--- pb-app/app/src/main/java/com/github/eeriefoods/pizzabrei/presentation/ui/upload/UploadScreen.kt
+++ pb-app/app/src/main/java/com/github/eeriefoods/pizzabrei/presentation/ui/upload/UploadScreen.kt
@@ -7,18 +7,19 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.os.BuildCompat.*
 import androidx.navigation.NavController
-import com.github.eeriefoods.pizzabrei.presentation.ui.navigation.Screens
 import com.github.eeriefoods.pizzabrei.presentation.theme.PizzaBreiTheme
+import com.github.eeriefoods.pizzabrei.presentation.ui.navigation.Screens
 
 
 @Composable
@@ -50,64 +51,83 @@ fun uploadScreen(navController: NavController) {
             mutableStateOf<Uri?>(null)
         }
 
-        Column() {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
 
-            TextField(
-                value = appName,
-                onValueChange = {
-                    appName = it
-                },
-                label = { Text("Name")},
-                placeholder = { Text("App Name")}
-            )
+            item {
+                TextField(
+                    value = appName,
+                    onValueChange = {
+                        appName = it
+                    },
+                    label = { Text("Name") },
+                    placeholder = { Text("App Name") },
+                    singleLine = true
+                )
+            }
 
-            TextField(
-                value = author,
-                onValueChange = {
-                    author = it
-                },
-                label = { Text("Author")},
-                placeholder = { Text("Author 1, Author 2, ...")}
-            )
+            item {
+                TextField(
+                    value = author,
+                    onValueChange = {
+                        author = it
+                    },
+                    label = { Text("Author") },
+                    placeholder = { Text("Author 1, Author 2, ...") }
+                )
+            }
 
-            TextField(
-                value = version,
-                onValueChange = {
-                    version = it
-                },
-                label = { Text("Version") },
-                placeholder = { Text("0.3.9") }
-            )
+            item {
+                TextField(
+                    value = version,
+                    onValueChange = {
+                        version = it
+                    },
+                    label = { Text("Version") },
+                    placeholder = { Text("0.3.9") },
+                    singleLine = true
+                )
+            }
 
-            TextField(
-                value = description,
-                onValueChange = {
-                    description = it
-                },
-                label = { Text("Beschreibung") }
-            )
+
+            item {
+                TextField(
+                    value = description,
+                    onValueChange = {
+                        description = it
+                    },
+                    label = { Text("Beschreibung") }
+                )
+            }
 
             //PhotoPickerExample()
 
-            Button(onClick = {
-                pickPictureLauncher.launch("image/*")
-            }) {
-                Text("Image Picker!")
+            item {
+                Button(onClick = {
+                    pickPictureLauncher.launch("image/*")
+                }) {
+                    Text("Image Picker!")
+                }
             }
 
-            Button(onClick = {
-                pickPictureLauncher.launch("downloads/*")
-            }) {
-                Text("File Picker!")
+            item {
+                Button(onClick = {
+                    pickPictureLauncher.launch("downloads/*")
+                }) {
+                    Text("File Picker!")
+                }
             }
 
-            Button(onClick = {
-                navController.navigate(Screens.Home.route)
-            }) {
-                Text("GO BACK!!!!!")
+            item {
+                Button(onClick = {
+                    navController.navigate(Screens.Home.route)
+                }) {
+                    Text("GO BACK!!!!!")
+                }
             }
-
-
         }
 
     }
